11d971562c55298619937195d28c18b7eac6592a
DeveloperKubilay <107062509+DeveloperKubilay@users.noreply.github.com>
2023-05-25 04:11:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const kubitdb = require("kubitdb")
 const db = new kubitdb("data")
 const express = require("express")
 const app = express();
-app.listen(process.env.PORT || 3001);
+app.listen(process.env.PORT || 3000);
 app.get('/', function (req, res) {res.send('Uptimer')})
 console.log("Uptime ready")
 var time = 0
